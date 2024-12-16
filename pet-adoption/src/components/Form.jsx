201174f5b92f476de9555144fac63163cd67e026
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-12-16 19:49:37 +0530

    Refactor form component to use external validation schema; remove inline validation logic

--- pet-adoption/src/components/Form.jsx
+++ pet-adoption/src/components/Form.jsx
@@ -1,31 +1,5 @@
 import {useRef} from "react"
-import {z} from "zod";
-
-const formSchema  = z.object({
-    petName: z.string().min(3,{
-        message: "Minimum 3 Character is Required"
-    }).max(50,{
-        message: "maximum 50 characters is allowed"
-    }),
-    petType: z.string(),
-    breed: z.string().min(3,{
-        message: "Minimum 3 Character is Required"
-    }).max(50,{
-        message: "maximum 50 characters is allowed"
-    }),
-    adopterName: z.string().min(3,{
-        message: "Minimum 3 Character is Required"
-    }).max(50,{
-        message: "maximum 50 characters is allowed"
-    }),
-    email: z.string().email("Invalid Email"),
-    phone: z.string().regex(/^\d{10}$/, { message: "Number must be of 10 Digit" })
-})
-
-
-
-
-
+import {formSchema} from '../utility/validation.js'
 
 export default function Form({setGoBack, setAdoptionTable}){
     const formRef = useRef({
