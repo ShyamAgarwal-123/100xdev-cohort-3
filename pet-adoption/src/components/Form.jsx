9aff491b8ef2937dd074384662226ccf38333a65
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-12-16 15:43:46 +0530

    Add Zod validation schema to form; update dependencies and improve form handling

--- pet-adoption/src/components/Form.jsx
+++ pet-adoption/src/components/Form.jsx
@@ -1,25 +1,58 @@
 import {useRef} from "react"
-import validator from "../utility/validation.js";
+import {z} from "zod";
+
+const formSchema  = z.object({
+    petName: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    petType: z.string(),
+    breed: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    adopterName: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    email: z.string().email("Invalid Email"),
+    phone: z.string().regex(/^\d{10}$/, { message: "Number must be of 10 Digit" })
+})
+
+
+
+
+
 
 export default function Form({setGoBack, setAdoptionTable}){
     const formRef = useRef({
         petName: "",
         petType: "",
-        breed: "",
+        breed: "Dog",
         adopterName: "",
         email: "",
         phone: ""
     })
     const handelChange = function(event){
         const {name, value} = event.target;
-
+        console.log(formSchema.safeParse(formRef));
         formRef[name] = value
     }
 
     const submitForm = function(event){  
         event.preventDefault()
         console.log(event);
-        setGoBack((goBack)=>!goBack)       
+        console.log(formSchema.safeParse(formRef));
+        if (formSchema.safeParse(formRef).success) {
+            setAdoptionTable((prevData)=>[...prevData,formRef])
+            setGoBack((goback)=> !goback) 
+            return
+        }
+        alert("All fields are Requied")
+             
     }
     
 
@@ -33,9 +66,9 @@ export default function Form({setGoBack, setAdoptionTable}){
         <div>
             <label htmlFor="petName">Pet Type</label>
             <select name="petType" onChange={handelChange} >
-                <option value="">A</option>
-                <option value="">B</option>
-                <option value="">C</option>
+                <option value="Dog">Dog</option>
+                <option value="Cat">Cat</option>
+                <option value="Rabit">Rabit</option>
             </select>
         </div>
         <div>
--- pet-adoption/src/utility/validation.js
+++ pet-adoption/src/utility/validation.js
@@ -1,11 +1,22 @@
-export default function validator({petName,petType,breed,adopterName,email,phone}) {
+import {z} from "zod";
 
-    console.log(petName,petType,breed,adopterName,email,phone);
-    
-    if(petName == false || petType == false ||
-        breed == false || adopterName == false || email == false
-        || phone == false
-    )  {return "Field is required"}
-
-   
-}
+export const formSchema  = z.object({
+    petName: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    petType: z.string(),
+    breed: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    adopterName: z.string().min(3,{
+        message: "Minimum 3 Character is Required"
+    }).max(50,{
+        message: "maximum 50 characters is allowed"
+    }),
+    email: z.string().email("Invalid Email"),
+    phone: z.string().regex(/^\d{10}$/, { message: "Number must be of 10 Digit" })
+})
