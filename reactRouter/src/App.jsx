46f71244f673bbacba9ec2848c9c15928e1b88c8
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-12-26 15:33:55 +0530

    Set up Tailwind CSS with Vite; add initial app structure and components for routing

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
